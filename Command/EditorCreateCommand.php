1fd88947b2c64b0d02a10b0be706da64dd4f9974
Marco Vito Moscaritolo <marco@agavee.com>
2014-01-04 16:32:19 +0100

    Update namespace.

--- Command/EditorCreateCommand.php
+++ Command/EditorCreateCommand.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Mavimo\Sculpin\Bundle\EditorBundle;
+namespace Mavimo\Sculpin\Bundle\EditorBundle\Command;
 
 use Sculpin\Core\Console\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
