54361bdc3dee50c0108143e2b3eb12057acdad8f
Marco Vito Moscaritolo <marco@agavee.com>
2014-01-04 16:10:28 +0100

    Added first stub of create command bundle.

---
+++ Command/EditorCreate.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mavimo\Sculpin\Bundle\EditorBundle;
+
+use Sculpin\Core\Console\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Sculpin Editor Bundle.
+ *
+ * @author Marco Vito Moscaritolo <marco@mavimo.org>
+ */
+class EditorCreate extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('create:new')
+            ->setDescription('Create a new content.')
+            ->setHelp("The <info>create:new</info> command create a new post content.");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Test");
+    }
+}
---
+++ SculpinEditorBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mavimo\Sculpin\Bundle\EditorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Sculpin Editor Bundle.
+ *
+ * @author Marco Vito Moscaritolo <marco@mavimo.org>
+ */
+class SculpinEditorBundle extends Bundle
+{
+
+}
